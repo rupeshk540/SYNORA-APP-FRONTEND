7131660e4d7e61848de049109bd2d4a2ce6ef38c
rupeshk540 <rrupesh80844@gmail.com>
2026-07-27 09:23:22 +0530

    Deployment Configurations

--- src/services/AxiosHelper.js
+++ src/services/AxiosHelper.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const baseURL ="http://localhost:8080";
+export const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
 
 export const httpClient = axios.create({
     baseURL: baseURL,
